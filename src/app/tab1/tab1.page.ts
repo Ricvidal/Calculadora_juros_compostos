65765b99038aab90b52ae0a5b237d5bf2a54b05a
Ricvidal <ricardoqvidal@gmail.com>
2021-11-18 22:58:02 -0300

    Back com toFixed ok

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
   resultado: number;
@@ -14,13 +14,16 @@ export class Tab1Page {
   constructor() {
     this.resultado = 0;
     this.principal = 10;
-    this.taxa = 0.5;
-    this.prazo = 1; 
+    this.taxa = 1;
+    this.prazo = 1;
   }
 
-  calcular(){
-    this.resultado=this.principal*(Math.pow((1+this.taxa/100), this.prazo));
+  calcular() {
+    let resultadoComFixed = 0;
+    resultadoComFixed =
+      this.principal * Math.pow(1 + this.taxa / 100, this.prazo);
+    
+    this.resultado = parseFloat(resultadoComFixed.toFixed(2));
     return this.resultado;
   }
-
 }
