208601a0d50a3a887b344caa11ae738c7d7633bb
Ricvidal <ricardoqvidal@gmail.com>
2021-11-18 22:47:52 -0300

    Back ok

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  resultado: number;
+  principal: number;
+  taxa: number;
+  prazo: number;
 
-  constructor() {}
+  constructor() {
+    this.resultado = 0;
+    this.principal = 10;
+    this.taxa = 0.5;
+    this.prazo = 1; 
+  }
+
+  calcular(){
+    this.resultado=this.principal*(Math.pow((1+this.taxa/100), this.prazo));
+    return this.resultado;
+  }
 
 }
